09ff3e6fd626208c03578cd49ffc04c7e610ab7f
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2025-01-02 11:32:57 -0500

    Fix output

--- aws/terraform/output.tf
+++ aws/terraform/output.tf
@@ -2,7 +2,7 @@ output "cluster_address" {
   value = "http://${aws_instance.server[0].public_ip}:4646"
 }
 
-output "server_lb_ip" {
-  value = aws_elb.server_lb.dns_name
-}
+# output "server_lb_ip" {
+#   value = aws_elb.server_lb.dns_name
+# }
 
