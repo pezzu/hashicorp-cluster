468fd5477ac330cc127bab328d95f12881631c37
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2025-01-06 18:10:35 -0500

    More outputs added

--- aws/terraform/output.tf
+++ aws/terraform/output.tf
@@ -1,5 +1,27 @@
-output "cluster_address" {
+output "nomad_address" {
   value = "http://${aws_instance.server[0].public_ip}:4646"
+}
+
+output "consul_address" {
+  value = "http://${aws_instance.server[0].public_ip}:8500"
+}
+
+output "vault_address" {
+  value = "http://${aws_instance.server[0].public_ip}:8200"
+}
+
+output "configuration" {
+  value = <<EOF
+UI is available at:
+Nomad  :  http://${aws_instance.server[0].public_ip}:4646/ui
+Consul :  http://${aws_instance.server[0].public_ip}:8500/ui
+Vault  :  http://${aws_instance.server[0].public_ip}:8200/ui
+
+CLI configuration:
+export NOMAD_ADDR=http://${aws_instance.server[0].public_ip}:4646
+export CONSUL_HTTP_ADDR=http://${aws_instance.server[0].public_ip}:8500
+export VAULT_ADDR=http://${aws_instance.server[0].public_ip}:8200
+EOF
 }
 
 # output "server_lb_ip" {
